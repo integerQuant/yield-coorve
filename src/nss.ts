0d11679d37a7775740dfef5f0ae37b965bd18fee
internQuant <cinzeis-rehang.0l@icloud.com>
2025-10-27 15:11:36 -0300

    change forward computation to closed simplied form

--- src/nss.ts
+++ src/nss.ts
@@ -20,30 +20,16 @@ export function computeNSSCurve(
 
   const term2 = tSafe.map((t, i) => (1 - exp_l1_t[i]) / (l1 * t));
   const term3 = term2.map((v, i) => v - exp_l1_t[i]);
-  const term4 = tSafe.map(
-    (t, i) => (1 - exp_l2_t[i]) / (l2 * t) - exp_l2_t[i]
+  const term4 = tSafe.map((t, i) => (1 - exp_l2_t[i]) / (l2 * t) - exp_l2_t[i]);
+
+  const spot = tSafe.map((_t, i) => b1 + b2 * term2[i] + b3 * term3[i] + b4 * term4[i]);
+
+  const forward = tenors.map((t, i) =>
+    b1
+    + b2 * exp_l1_t[i]
+    + b3 * l1 * t * exp_l1_t[i]
+    + b4 * l2 * t * exp_l2_t[i]
   );
 
-  const y_tau = tSafe.map(
-    (_t, i) => b1 + b2 * term2[i] + b3 * term3[i] + b4 * term4[i]
-  );
-
-  // Derivatives w.r.t. tau (tenor) for instantaneous forward calculation
-  const dterm2_dtau = tSafe.map(
-    (t, i) => (l1 * t * exp_l1_t[i] - (1 - exp_l1_t[i])) / (l1 * t * t)
-  );
-  const dterm3_dtau = tSafe.map((_, i) => dterm2_dtau[i] + l1 * exp_l1_t[i]);
-  const dterm4_dtau = tSafe.map(
-    (t, i) =>
-      ((l2 * t * exp_l2_t[i] - (1 - exp_l2_t[i])) / (l2 * t * t)) +
-      l2 * exp_l2_t[i]
-  );
-
-  const dy_dtau = tSafe.map(
-    (_t, i) => b2 * dterm2_dtau[i] + b3 * dterm3_dtau[i] + b4 * dterm4_dtau[i]
-  );
-
-  const f_tau = tSafe.map((t, i) => y_tau[i] + t * dy_dtau[i]);
-
-  return { t: tenors, spot: y_tau, forward: f_tau };
+  return { t: tenors, spot, forward };
 }
